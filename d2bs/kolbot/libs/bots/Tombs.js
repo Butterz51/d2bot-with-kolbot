026c8f1dea5282682d03c0f3c6d85c3d6af57372
Butterz51 <justin86@live.ca>
2021-03-07 09:26:43 -0500

    Update Tombs.js
    This edit allows public or private runs that allows you to select target types (normal, all, bosses)
    It also gives you the option of walking or teleporting towards or at targets selected
    When set to public runs it will clear all Tombs, in addition, BO occurs at the start of each Tomb
    When set to private BO only occurs when needed
    
    ** JBH **

--- d2bs/kolbot/libs/bots/Tombs.js
+++ d2bs/kolbot/libs/bots/Tombs.js
@@ -2,6 +2,11 @@
 *	@filename	Tombs.js
 *	@author		kolton
 *	@desc		clear Tal Rasha's Tombs
+*				This edit allows public or private runs that allows you to select target types (normal, all, bosses)
+*				It also gives you the option of walking or teleporting towards or at targets selected
+*				When set to public runs it will clear all Tombs, in addition BO occurs at the start of each Tomb
+*				When set to private BO only occurs when needed
+*	@edited		JBH
 */
 
 function Tombs() {
@@ -15,17 +20,30 @@ function Tombs() {
 		if (!Pather.moveToExit(i, true)) {
 			throw new Error("Failed to move to tomb");
 		}
-
-		Attack.clearLevel(Config.ClearType);
-
+		if (Config.Tombs.WalkClear){
+			Pather.teleport = !Config.Tombs.WalkClear && Pather._teleport;
+		}
+		if (Config.Tombs.PortalLeech) {{
+			Pather.makePortal();
+			say(Config.Tombs.TPMessage);
+			delay(10000);
+			Precast.doPrecast(true);
+			delay(100);	
+		}
+		if (Config.Tombs.PortalLeech) {
+			Attack.clearLevel(0);
+			Pickit.pickItems();
+		}	
+		}else{	
+			Attack.clearLevel(Config.ClearType2);
+			Pickit.pickItems	
+		}
 		if (i === 69) {
 			Precast.doPrecast(true);
 		}
-
 		if (!Pather.moveToExit(46, true)) {
 			throw new Error("Failed to move to Canyon");
 		}
 	}
-
 	return true;
 }
